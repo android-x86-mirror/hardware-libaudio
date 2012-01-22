2d37c6cf645f282661995c21106920ef587b1034
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-01-23 03:18:19 +0800

    audio hal for ics-x86
    Modified from Linaro project.

--- Android.mk
+++ Android.mk
@@ -11,3 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := audio.primary.default
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_SHARED_LIBRARIES := \
+    libdl \
+    libmedia \
+    libutils \
+    libcutils \
+    libhardware_legacy
+
+LOCAL_SRC_FILES := \
+    alsa_pcm.c \
+    alsa_mixer.c \
+    AudioHardware.cpp
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiohw_legacy
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
